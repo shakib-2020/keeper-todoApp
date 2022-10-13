05e4c7dbaa97a03b8e7fd83babe4f65d3e17f188
Md Nazmus Shakib <nazshakib01@gmail.com>
2022-10-14 01:41:43 +0600

    add value to localStorage functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
+// get notes form localStorage
+
+function getLocalNotes() {
+  let list = localStorage.getItem("notesList");
+  if (list) {
+    return JSON.parse(localStorage.getItem("notesList"));
+  } else {
+    return [];
+  }
+}
+
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(getLocalNotes());
 
   function addNote(newNote) {
-    setNotes(prevNotes => {
+    setNotes((prevNotes) => {
       return [...prevNotes, newNote];
     });
   }
 
   function deleteNote(id) {
-    setNotes(prevNotes => {
+    setNotes((prevNotes) => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
       });
     });
   }
+
+  // add notes to localStorage
+  useEffect(() => {
+    localStorage.setItem("notesList", JSON.stringify(notes));
+  }, [notes]);
 
   return (
     <div>
